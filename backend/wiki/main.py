4877343322c4fb59e4b540b83d2fe15f59283a97
Egorov Mikhail <m-5321e@yandex.com>
2023-10-04 17:34:48 +0500

    Added swagger in docs

--- backend/wiki/main.py
+++ backend/wiki/main.py
@@ -1,5 +1,7 @@
 import uvicorn
+import yaml
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 from wiki.api import api_router
 from wiki.config import settings
@@ -17,6 +19,14 @@ api = FastAPI(
 # frontend = FastAPI()
 
 api.include_router(api_router)
+
+
+@api.get(f"/swagger.yaml", include_in_schema=False)
+async def get_swagger():
+    openapi_schema = get_openapi(title=f"{settings.PROJECT_NAME} API", version=settings.VERSION, routes=api.routes)
+    with open("./docs/swagger.yaml", "w") as file:
+        yaml.dump(openapi_schema, file)
+    return openapi_schema
 
 
 if __name__ == "__main__":
