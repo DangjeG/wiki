878e28e3e12df7e754d59bdc50528251f0876e79
Artur <fartur09@mail.ru>
2023-12-06 17:55:37 +0500

    export don't work

--- backend/wiki/export_document/router.py
+++ backend/wiki/export_document/router.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from lakefs_client.client import LakeFSClient
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
-from starlette.responses import FileResponse
+from starlette.responses import StreamingResponse
 
 from wiki.common.schemas import WikiUserHandlerData
 from wiki.database.deps import get_db
@@ -31,7 +31,7 @@ async def download_docx(
         user: WikiUserHandlerData = Depends(BasePermission(responsibility=ResponsibilityType.VIEWER))
 ):
     document_repository: DocumentRepository = DocumentRepository(session)
-    document = await document_repository.get_document_by_id(document_id, )
+    document = await document_repository.get_document_by_id(document_id, False)
 
     block_repository: BlockRepository = BlockRepository(session)
 
@@ -53,11 +53,13 @@ async def download_docx(
         )
         list_content_document.append(content)
 
-    file_name = str(user.id) + str(document_id)
+    file_name = "export"
 
-    await export_document(
+    bytes = await export_document(
         file_name=file_name,
         list_document_content=list_content_document
     )
 
-    return FileResponse("temp_export/" + file_name + ".docx", media_type='application/octet-stream', filename=file_name + ".docx")
+    headers = {"Content-Disposition": f'attachment; filename="{file_name}.docx"'}
+
+    return StreamingResponse(content=bytes.read(), headers=headers, media_type='application/msword')
--- backend/wiki/export_document/utils.py
+++ backend/wiki/export_document/utils.py
@@ -1,17 +1,29 @@
-import docx
-import spire
-from spire.doc import *
+from io import BytesIO
+
+from docx import Document as Doc
+from spire.doc import Document, FileFormat, XHTMLValidationType, Stream
 
 
-async def convert3(input_file, output_file):
-    document = spire.doc.Document()
+async def convert(data: str) -> BytesIO:
+    document = Document()
 
-    document.LoadFromFile(input_file, FileFormat.Html, XHTMLValidationType.none)
+    stream = Stream(data.encode(encoding='UTF-8'))
 
-    document.SaveToFile(output_file, FileFormat.Docx2016)
+    document.LoadFromStream(stream, FileFormat.Html, XHTMLValidationType.none)
+
+    result_stream = Stream()
+
+    document.SaveToStream(result_stream)
+
+    array_bytes = result_stream.ToArray()
+
+    result_bytes = b''.join(array_bytes)
+
     document.Close()
 
-    await remove_watermark(output_file, docx.Document(output_file))
+    result_bytes = await remove_watermark(Doc(BytesIO(result_bytes)))
+
+    return result_bytes
 
 
 async def delete_paragraph(paragraph):
@@ -20,14 +32,18 @@ async def delete_paragraph(paragraph):
     paragraph._p = paragraph._element = None
 
 
-async def remove_watermark(file_name: str, doc):
+async def remove_watermark(doc: Doc):
     i = 0
     while i < len(doc.paragraphs):
         if 'Evaluation Warning: The document was created with Spire.Doc for Python.' in doc.paragraphs[i].text:
             for j in range(1):
                 await delete_paragraph(doc.paragraphs[j])
         i += 1
-    doc.save(file_name)
+    result_bytes = BytesIO()
+
+    doc.save(result_bytes)
+
+    return result_bytes
 
 
 async def export_document(file_name: str, list_document_content: list[str]):
@@ -35,7 +51,7 @@ async def export_document(file_name: str, list_document_content: list[str]):
         <html>
          <head>
           <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-          <title>Пример веб-страницы</title>
+          <title></title>
          </head>
          <body>"""
 
@@ -45,9 +61,4 @@ async def export_document(file_name: str, list_document_content: list[str]):
     html += """</body>
         </html>"""
 
-    os.mkdir("temp_export")
-
-    with open("temp_export/" + file_name + ".html", "w", encoding="utf-8") as file:
-        file.write(html)
-
-    await convert3("temp_export/" + file_name + ".html", "temp_export/" + file_name + ".docx")
+    return await convert(html)
